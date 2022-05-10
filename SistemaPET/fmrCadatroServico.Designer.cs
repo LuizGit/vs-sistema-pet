7f90b91b5189579788f05142afbc69c8938e7b35
Luiz Antonio <luiz.workmail@gmail.com>
2022-05-10 17:03:01 -0300

    Luiz Antonio - Tela Cadastro de Serviço - Iniciado a parte de layout da tela.

---
+++ SistemaPET/fmrCadatroServico.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace SistemaPET
+{
+    partial class fmrCadatroServico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gpbDados = new System.Windows.Forms.GroupBox();
+            this.gpbPesquisa = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvPesquisaServico = new System.Windows.Forms.DataGridView();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gpbDados.SuspendLayout();
+            this.gpbPesquisa.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPesquisaServico)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gpbDados
+            // 
+            this.gpbDados.Controls.Add(this.txtPreco);
+            this.gpbDados.Controls.Add(this.label3);
+            this.gpbDados.Controls.Add(this.btnCancelar);
+            this.gpbDados.Controls.Add(this.btnExcluir);
+            this.gpbDados.Controls.Add(this.btnAlterar);
+            this.gpbDados.Controls.Add(this.btnCadastrar);
+            this.gpbDados.Controls.Add(this.txtDescricao);
+            this.gpbDados.Controls.Add(this.label1);
+            this.gpbDados.Location = new System.Drawing.Point(7, 5);
+            this.gpbDados.Name = "gpbDados";
+            this.gpbDados.Size = new System.Drawing.Size(311, 316);
+            this.gpbDados.TabIndex = 0;
+            this.gpbDados.TabStop = false;
+            this.gpbDados.Text = "Dados";
+            // 
+            // gpbPesquisa
+            // 
+            this.gpbPesquisa.Controls.Add(this.dgvPesquisaServico);
+            this.gpbPesquisa.Controls.Add(this.textBox2);
+            this.gpbPesquisa.Controls.Add(this.label2);
+            this.gpbPesquisa.Location = new System.Drawing.Point(324, 5);
+            this.gpbPesquisa.Name = "gpbPesquisa";
+            this.gpbPesquisa.Size = new System.Drawing.Size(338, 316);
+            this.gpbPesquisa.TabIndex = 1;
+            this.gpbPesquisa.TabStop = false;
+            this.gpbPesquisa.Text = "Pesquisa";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Descrição";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(22, 53);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(283, 20);
+            this.txtDescricao.TabIndex = 1;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(214, 152);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.TabIndex = 2;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(118, 152);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 3;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(21, 152);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 4;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(118, 218);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 5;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(40, 37);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(283, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Descrição";
+            // 
+            // dgvPesquisaServico
+            // 
+            this.dgvPesquisaServico.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPesquisaServico.Location = new System.Drawing.Point(6, 74);
+            this.dgvPesquisaServico.Name = "dgvPesquisaServico";
+            this.dgvPesquisaServico.Size = new System.Drawing.Size(332, 239);
+            this.dgvPesquisaServico.TabIndex = 2;
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(21, 92);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(84, 20);
+            this.txtPreco.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Preço";
+            // 
+            // fmrCadatroServico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 330);
+            this.Controls.Add(this.gpbPesquisa);
+            this.Controls.Add(this.gpbDados);
+            this.Name = "fmrCadatroServico";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Serviço";
+            this.gpbDados.ResumeLayout(false);
+            this.gpbDados.PerformLayout();
+            this.gpbPesquisa.ResumeLayout(false);
+            this.gpbPesquisa.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPesquisaServico)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gpbDados;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gpbPesquisa;
+        private System.Windows.Forms.DataGridView dgvPesquisaServico;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
