5aae34717f98e7ba22d4f4993721126a9e3bcb2d
Luiz Antonio <luiz.workmail@gmail.com>
2022-05-30 23:33:48 -0300

    Luiz Antonio - Tela Cadastro Pacote - Alterações de layout.

--- SistemaPET/fmrCadastroPacote.Designer.cs
+++ SistemaPET/fmrCadastroPacote.Designer.cs
@@ -30,25 +30,25 @@ namespace SistemaPET
         private void InitializeComponent()
         {
             this.dpgDados = new System.Windows.Forms.GroupBox();
-            this.gpbItens = new System.Windows.Forms.GroupBox();
-            this.dgvItensPacote = new System.Windows.Forms.DataGridView();
-            this.button2 = new System.Windows.Forms.Button();
-            this.txtVlrTotalPct = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
+            this.txtDescontoItem = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnPesquisar = new System.Windows.Forms.Button();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.btnPesquisar = new System.Windows.Forms.Button();
-            this.btnAdicionar = new System.Windows.Forms.Button();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gpbItens = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtVlrTotalPct = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dgvItensPacote = new System.Windows.Forms.DataGridView();
             this.dpgDados.SuspendLayout();
             this.gpbItens.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvItensPacote)).BeginInit();
@@ -56,7 +56,7 @@ namespace SistemaPET
             // 
             // dpgDados
             // 
-            this.dpgDados.Controls.Add(this.textBox6);
+            this.dpgDados.Controls.Add(this.txtDescontoItem);
             this.dpgDados.Controls.Add(this.label7);
             this.dpgDados.Controls.Add(this.btnAdicionar);
             this.dpgDados.Controls.Add(this.btnPesquisar);
@@ -77,123 +77,39 @@ namespace SistemaPET
             this.dpgDados.TabStop = false;
             this.dpgDados.Text = "Serviços / Produtos";
             // 
-            // gpbItens
+            // txtDescontoItem
             // 
-            this.gpbItens.Controls.Add(this.label1);
-            this.gpbItens.Controls.Add(this.txtVlrTotalPct);
-            this.gpbItens.Controls.Add(this.button2);
-            this.gpbItens.Controls.Add(this.dgvItensPacote);
-            this.gpbItens.Location = new System.Drawing.Point(3, 187);
-            this.gpbItens.Name = "gpbItens";
-            this.gpbItens.Size = new System.Drawing.Size(935, 382);
-            this.gpbItens.TabIndex = 1;
-            this.gpbItens.TabStop = false;
-            this.gpbItens.Text = "Itens do Pacote";
+            this.txtDescontoItem.Location = new System.Drawing.Point(512, 109);
+            this.txtDescontoItem.Name = "txtDescontoItem";
+            this.txtDescontoItem.Size = new System.Drawing.Size(100, 20);
+            this.txtDescontoItem.TabIndex = 13;
             // 
-            // dgvItensPacote
+            // label7
             // 
-            this.dgvItensPacote.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvItensPacote.Location = new System.Drawing.Point(6, 22);
-            this.dgvItensPacote.Name = "dgvItensPacote";
-            this.dgvItensPacote.ReadOnly = true;
-            this.dgvItensPacote.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvItensPacote.Size = new System.Drawing.Size(876, 314);
-            this.dgvItensPacote.TabIndex = 0;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(451, 112);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Desconto:";
             // 
-            // button2
+            // btnAdicionar
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.Red;
-            this.button2.Location = new System.Drawing.Point(888, 43);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(41, 39);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "X";
-            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Location = new System.Drawing.Point(792, 134);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionar.TabIndex = 11;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
             // 
-            // txtVlrTotalPct
+            // btnPesquisar
             // 
-            this.txtVlrTotalPct.Location = new System.Drawing.Point(782, 348);
-            this.txtVlrTotalPct.Name = "txtVlrTotalPct";
-            this.txtVlrTotalPct.ReadOnly = true;
-            this.txtVlrTotalPct.Size = new System.Drawing.Size(100, 20);
-            this.txtVlrTotalPct.TabIndex = 3;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(666, 355);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Valor Total do Pacote";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(44, 25);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Descrição:";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(108, 22);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(284, 20);
-            this.textBox2.TabIndex = 1;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(108, 74);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(363, 20);
-            this.textBox1.TabIndex = 3;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(72, 77);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(30, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Item:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(73, 112);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(56, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Valor item:";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(130, 109);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 5;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(308, 109);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 7;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(257, 112);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(50, 13);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "Qtd Item:";
+            this.btnPesquisar.Location = new System.Drawing.Point(502, 72);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btnPesquisar.TabIndex = 10;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
             // 
             // textBox5
             // 
@@ -212,39 +128,123 @@ namespace SistemaPET
             this.label6.TabIndex = 8;
             this.label6.Text = "Valor Total item:";
             // 
-            // btnPesquisar
+            // textBox4
             // 
-            this.btnPesquisar.Location = new System.Drawing.Point(502, 72);
-            this.btnPesquisar.Name = "btnPesquisar";
-            this.btnPesquisar.Size = new System.Drawing.Size(75, 23);
-            this.btnPesquisar.TabIndex = 10;
-            this.btnPesquisar.Text = "Pesquisar";
-            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.textBox4.Location = new System.Drawing.Point(308, 109);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
             // 
-            // btnAdicionar
+            // label5
             // 
-            this.btnAdicionar.Location = new System.Drawing.Point(792, 134);
-            this.btnAdicionar.Name = "btnAdicionar";
-            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
-            this.btnAdicionar.TabIndex = 11;
-            this.btnAdicionar.Text = "Adicionar";
-            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(257, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Qtd Item:";
             // 
-            // textBox6
+            // textBox3
             // 
-            this.textBox6.Location = new System.Drawing.Point(512, 109);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 20);
-            this.textBox6.TabIndex = 13;
+            this.textBox3.Location = new System.Drawing.Point(130, 109);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
             // 
-            // label7
+            // label4
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(451, 112);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(56, 13);
-            this.label7.TabIndex = 12;
-            this.label7.Text = "Desconto:";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Valor item:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(363, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Item:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(108, 22);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(284, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Descrição:";
+            // 
+            // gpbItens
+            // 
+            this.gpbItens.Controls.Add(this.label1);
+            this.gpbItens.Controls.Add(this.txtVlrTotalPct);
+            this.gpbItens.Controls.Add(this.button2);
+            this.gpbItens.Controls.Add(this.dgvItensPacote);
+            this.gpbItens.Location = new System.Drawing.Point(3, 187);
+            this.gpbItens.Name = "gpbItens";
+            this.gpbItens.Size = new System.Drawing.Size(935, 382);
+            this.gpbItens.TabIndex = 1;
+            this.gpbItens.TabStop = false;
+            this.gpbItens.Text = "Itens do Pacote";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(666, 355);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Valor Total do Pacote";
+            // 
+            // txtVlrTotalPct
+            // 
+            this.txtVlrTotalPct.Location = new System.Drawing.Point(782, 348);
+            this.txtVlrTotalPct.Name = "txtVlrTotalPct";
+            this.txtVlrTotalPct.ReadOnly = true;
+            this.txtVlrTotalPct.Size = new System.Drawing.Size(100, 20);
+            this.txtVlrTotalPct.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(888, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(41, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "X";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dgvItensPacote
+            // 
+            this.dgvItensPacote.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvItensPacote.Location = new System.Drawing.Point(6, 22);
+            this.dgvItensPacote.Name = "dgvItensPacote";
+            this.dgvItensPacote.ReadOnly = true;
+            this.dgvItensPacote.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvItensPacote.Size = new System.Drawing.Size(876, 314);
+            this.dgvItensPacote.TabIndex = 0;
             // 
             // fmrCadastroPacote
             // 
@@ -267,7 +267,7 @@ namespace SistemaPET
         #endregion
 
         private System.Windows.Forms.GroupBox dpgDados;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox txtDescontoItem;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button btnAdicionar;
         private System.Windows.Forms.Button btnPesquisar;
